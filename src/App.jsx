86933ab67e9be46ab8fe50b920a2f27c9730ab8c
AbdullahMansoor700 <abdullah.mansoor@cloudmizan.com>
2024-07-05 00:17:20 +0500

    all changes

--- src/App.jsx
+++ src/App.jsx
@@ -22,6 +22,35 @@ const exponential_numbers=numbers.map(function (number){
     return number*number;
 });
 
+ 
+// PRACTICE WITH JAVA FUNCTIONS
+
+// const user={
+//   id : 1,
+//   name:{
+//     first:abdullah,
+//     last:masnoor
+//   },
+//   contact:{
+//     home:'03xx',
+//     work:'042xx'
+//   }
+//  };
+
+//  const{
+//   id,
+//   name:{
+//     first,
+//     last
+//   },
+//   contact:{
+//     home,
+//     work
+//   }
+
+// }=user;
+//..................END
+ 
 // const list1=[
 //   {
 //     title: 'react',
@@ -61,32 +90,29 @@ const exponential_numbers=numbers.map(function (number){
 //     objectID: 12,    
 //   },
 // ];
-//component declaration
-const Item=({item})=>(
-  <li >
-  <span> <a href={item.url}>{item.title}</a> </span>
-  <span> {item.author} </span>
-  <span> {item.num_comments} </span>
-  <span> {item.points} </span>
-  </li>
-)
-//component declaration
-const Lists=({list})=>{
-console.log("list renders");
-  return(
-    <ul>
-      {list.map((item)=>(
-         <Item key={item.objectID} item = {item} />
 
-      ))
-    }
-    </ul>
-  )
-  }
+//component declaration
+const List = ({ list }) => {
+  return(
+   <ul>
+        {list.map((item) => (
+           <Item key={item.objectID} item={item} />
+         ))}
+  </ul>
+  );
+};
+const Item = ({ item }) => (
+   <li>
+      <span> <a href={item.url}>{item.title}</a></span>
+      <span>{item.author}</span>
+      <span>{item.num_comments}</span>
+      <span>{item.points}</span>
+  </li>
+);
+  
 //component declared
 const Search=({search,onSearch})=>{
 console.log("search renders");
-
   return( 
     <div>
     <label htmlFor='search'> Search </label>
@@ -139,9 +165,21 @@ console.log("app renders");
   ];
 //call back handler concept (but here it is event handler in app component)
 
-  const[searchTerm,setSearchTerm]=React.useState('')
+  const[searchTerm,setSearchTerm]=React.useState(
+    localStorage.getItem('search') || 'React'
+  )
+  //a side effect refers ti the interaction with outside world, we use reacts useHook state to encounter it
+  // useEfeect hook is useed to perform sideeffects
+  //the dependancy array basically specify when side eeffect should run
+  React.useEffect(
+    ()=>{localStorage.setItem('search',searchTerm)},[searchTerm]
+  )
+
   const handleSearch=(event)=>{
     setSearchTerm(event.target.value);
+    //first it was used here but the problem was 
+    //we dont have acces to setSearchterm function here its out of scope
+    // localStorage.setItem('search',event.target.value)
   };
 
   const searchStories=stories.filter((story)=>
@@ -154,8 +192,8 @@ console.log("app renders");
 {/* component instantiated */}
      <Search onSearch={handleSearch} search={searchTerm}/>
       <hr />
-{/* xomponent instantiated */}
-      <Lists list={searchStories} />
+{/* component instantiated */}
+      <List list={searchStories} />
       {/* component instantiated */}
       {/* <Lists list={list2}/> */}
     </div>
